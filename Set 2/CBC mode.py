78173186b86f84da92ead187d2e1495b14c23ae8
Sanjana Sarda <31695528+Sanjana-Sarda@users.noreply.github.com>
2020-06-29 11:47:42 -0700

    Update CBC mode.py

--- Set 2/CBC mode.py
+++ Set 2/CBC mode.py
@@ -1,5 +1,4 @@
 from Cryptodome.Cipher import AES
-from Cryptodome.Util.Padding import pad, unpad
 import base64
 
 def xor (b, c): 
@@ -13,14 +12,31 @@ def ecb_decode(text):
     cipher = AES.new(key, AES.MODE_ECB)
     return(cipher.decrypt(text))
 
+def ecb_encode(text):
+    key = b'YELLOW SUBMARINE'
+    cipher = AES.new(key, AES.MODE_ECB)
+    return(cipher.encrypt(text))
+
+def cbc_decode(text, IV, BS):
+    de = bytearray(len(text))
+    pt = IV
+    for a in range (0, len(text), BS):
+        de[a:a+BS] = xor(pt, ecb_decode(text[a:a+BS]))
+        pt = text[a:a+BS]
+    return(de.decode('utf-8'))
+
+def cbc_encode(text, IV, BS):
+    sot = bytearray(text, 'utf-8')
+    pt = IV
+    en = bytearray(len(sot))
+    for a in range (0, len(sot), BS):
+        en[a:a+BS] = ecb_encode(xor(sot[a:a+BS], pt))
+        pt = en[a:a+BS]
+
+
 BS = 16
 cipher_text = open('10.txt').read()
 text = base64.b64decode(cipher_text)
 IV = bytes("0"*BS, 'utf-8')                               
-de = bytearray(len(text))
-pt = IV
-for a in range (0, len(text), BS):
-    de[a:a+BS] = xor(pt, ecb_decode(text[a:a+BS]))
-    pt = text[a:a+BS]
-print (de.decode('utf-8'))
-    
+so = cbc_decode(text, IV, BS)
+print (so)
